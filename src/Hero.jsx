13efe48eed3f346c56a7a565a2950dafba25066c
Saullo <saullorhamon@gmail.com>
2022-01-25 21:56:21 -0300

    add topo file image

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
         <div className="flex justify-center pt-6">
             <img
                 className="xl:w-1/2 xl:h-1/3 w-10/12"
-                src="../img/topo.png" alt="" />
+                src="../images/topo.png" alt="" />
         </div>
         <div className="font-poppins tracking-wider text-center font-bold text-4xl pl-9 py-4">Online Experiences</div>
         <div className="flex flex-wrap pl-9 pr-3 justify-center">
